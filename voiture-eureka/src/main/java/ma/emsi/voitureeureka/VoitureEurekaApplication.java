6130756de5cc039fe6f10226cad83b0d780e8d92
YoussefElmoudene <elmoudene.ysf@gmail.com>
2023-12-07 22:48:21 +0100

    first commit

--- voiture-eureka/src/main/java/ma/emsi/voitureeureka/VoitureEurekaApplication.java
+++ voiture-eureka/src/main/java/ma/emsi/voitureeureka/VoitureEurekaApplication.java
@@ -2,15 +2,16 @@ package ma.emsi.voitureeureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.stereotype.Component;
 
 @SpringBootApplication
-@EnableFeignClients
+@EnableDiscoveryClient
 @Component("org.emsi.voiture")
 public class VoitureEurekaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(VoitureEurekaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(VoitureEurekaApplication.class, args);
+    }
 
 }
